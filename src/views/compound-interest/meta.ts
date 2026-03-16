90907b269c45d5dd50c1b6e55c8f3dd0e9952934
plsgivemeachane <onewnew553@gmail.com>
2026-03-17 05:26:24 +0700

    fix: changing author name

--- src/views/compound-interest/meta.ts
+++ src/views/compound-interest/meta.ts
@@ -3,9 +3,9 @@ import type { PageMeta } from '@/types/page'
 const meta: PageMeta = {
   name: 'Tính Lãi Kép',
   description: 'Tính lãi suất kép - see your money grow over time with the magic of compounding',
-  author: 'J2TEAM',
+  author: 'Nekomi Akane',
   category: 'finance',
-  facebook: 'https://www.facebook.com/groups/j2team.community',
+  facebook: 'https://www.facebook.com/quanvndzai',
 }
 
 export default meta
